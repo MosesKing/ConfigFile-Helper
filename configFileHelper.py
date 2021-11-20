089472e9b6d8cf94563fb240f0a4197be4907c8c
Moesaeah King <mkc42@mail.missouri.edu>
2021-11-20 16:48:34 -0600

    made executable
    exectuable and script update

--- configFileHelper.py
+++ configFileHelper.py
@@ -6,17 +6,17 @@ def getFiles(inputFile, outputFile):
         for path, subdirs, files in os.walk(r'{}'.format(inputFile)):
             for filename in files:
                 f = os.path.join(path, filename)
-                a.write('"{}"{}'.format(str(f), os.linesep)) 
+                a.write('<file format='"CSV"' path="{}"/>{}'.format(str(f), os.linesep)) 
 
 def main():
 
-    outputFile = 'C:\dev\Personal Scripts\Python\output.txt'
+    outputFile = 'I:\ResultFiles\output.txt'
     inputFile = input("You'd like a written list of files from which directory (please put in full path): ")
 
     getFiles(inputFile, outputFile)
 
     print("List of files made")
-    print("Please see the created file on YOUR desktop! ")
+    print("Please see the created file on YOUR ResultFile Directory! ")
 
     input()
 
